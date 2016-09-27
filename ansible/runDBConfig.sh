317c96d5ca1f5769acdea8503d4d396021254798
Pablo Moreno <pablo.a.moreno@gmail.com>
2016-09-27 17:26:10 +0100

    Adds support for deployment with postgresql on a separate pod, using a secret to store and provide password.

---
+++ ansible/runDBConfig.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt-get update -y && sudo apt-get install -y --no-install-recommends ansible
+ansible-playbook -i "localhost," -c local ansible/set-galaxy-config-values.yaml
+echo "Configured DB connection succesfully"
