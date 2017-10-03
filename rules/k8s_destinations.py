0c795c647df9d51dd6edd138c757d5fa531e09b5
Pablo Moreno <pablo.a.moreno@gmail.com>
2017-10-03 09:11:29 +0100

    Includes default resources and docker enabled on destination code. First working version

--- rules/k8s_destinations.py
+++ rules/k8s_destinations.py
@@ -3,4 +3,5 @@ import os
 
 def k8s_wrapper(resource_params):
     # Allocate extra time
+    resource_params['docker_enabled'] = True
     return JobDestination(runner="k8s", params=resource_params)
