a4764fee509fd280da535e50f68f22bf5e1d8331
Pablo Moreno <pablo.a.moreno@gmail.com>
2017-10-18 16:21:50 +0100

    Adds k8s dynamic destination ability to load container assignment file.

--- rules/k8s_destinations.py
+++ rules/k8s_destinations.py
@@ -1,8 +1,12 @@
 from galaxy.jobs import JobDestination
+import yaml
+
 
 # Sizes for the different set of resources to be used in Kubernetes
 # CPU values are in CPU units (0.1 means 100 mili CPUs)
 # Memory values are in Gigabytes.
+
+# TODO: Read YAML file with mappings from here.
 
 __tiny = {'requests_cpu': 0.1,
           'limits_cpu': 0.5,
@@ -34,6 +38,7 @@ __xlarge = {'requests_cpu': 4,
             'limits_memory': 16
             }
 
+__path_tool2container = "config/phenomenal_tools2container.yaml"
 
 def k8s_wrapper(resource_params):
     # Allocate extra time
@@ -41,24 +46,37 @@ def k8s_wrapper(resource_params):
     return JobDestination(runner="k8s", params=resource_params)
 
 
-def k8s_wrapper_tiny(resource_params):
+def k8s_wrapper_tiny(resource_params, tool_id):
+    __read_assignments(resource_params,tool_id)
     return __setup_resources(resource_params, settings=__tiny)
 
 
-def k8s_wrapper_small(resource_params):
+def k8s_wrapper_small(resource_params, tool_id):
+    __read_assignments(resource_params, tool_id)
     return __setup_resources(resource_params, settings=__small)
 
 
-def k8s_wrapper_medium(resource_params):
+def k8s_wrapper_medium(resource_params, tool_id):
+    __read_assignments(resource_params, tool_id)
     return __setup_resources(resource_params, settings=__medium)
 
 
-def k8s_wrapper_large(resource_params):
+def k8s_wrapper_large(resource_params, tool_id):
+    __read_assignments(resource_params, tool_id)
     return __setup_resources(resource_params, settings=__large)
 
 
-def k8s_wrapper_xlarge(resource_params):
+def k8s_wrapper_xlarge(resource_params, tool_id):
+    __read_assignments(resource_params, tool_id)
     return __setup_resources(resource_params, settings=__xlarge)
+
+
+def __read_assignments(resource_params, tool_id):
+    stream = open(__path_tool2container, mode='r')
+    mappings = yaml.load(stream)
+    for mapping in mappings['assignment']:
+        if mapping['tool_id'] == tool_id:
+            resource_params.update(mapping)
 
 
 def __setup_resources(resource_params, settings):
@@ -81,7 +99,7 @@ def __merge_into_res_params(resource_params, settings, resource_type):
         resource_params['requests_' + resource_type] = settings['requests_' + resource_type]
         resource_params['limits_' + resource_type] = settings['limits_' + resource_type]
     elif resource_params['requests_' + resource_type] != 0 and resource_params['limits_' + resource_type] == 0:
-        resource_params['limits_' + resource_type] = max(resource_params['requests_' + resource_type] * 2,
+        resource_params['limits_' + resource_type] = max(resource_params['requests_' + resource_type],
                                                          settings['limits_' + resource_type])
     elif resource_params['requests_' + resource_type] == 0 and resource_params['limits_' + resource_type] != 0:
         resource_params['requests_' + resource_type] = min(settings['limits_' + resource_type],
