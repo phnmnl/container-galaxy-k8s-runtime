9b38e72cc57ebf62483b042cdff2db33425c2803
Luca Pireddu <pireddu@crs4.it>
2018-06-25 12:05:37 +0200

    Send all log messages to stderr

--- simplified_galaxy_stable_container_creation.sh
+++ simplified_galaxy_stable_container_creation.sh
@@ -11,20 +11,6 @@ TRUE='true'
 FALSE='false'
 DFT_DOCKER_PUSH_ENABLED=$TRUE
 
-# Error {{{1
-################################################################
-
-function error {
-
-	local msg=$1
-	local code=$2
-	[[ -z $code ]] && code=1
-
-	echo "[ERROR] $msg" >&2
-
-	exit $code
-}
-
 # Global variables {{{1
 ################################################################
 
@@ -77,6 +63,25 @@ POSTGRES_VERSION=$(grep FROM "${PG_Dockerfile}" | awk -F":" '{ print $2 }')
 POSTGRES_TAG=$DOCKER_REPO$DOCKER_USER/galaxy-postgres:$POSTGRES_VERSION"_for_"$GALAXY_VER_FOR_POSTGRES
 PROFTPD_TAG=$DOCKER_REPO$DOCKER_USER/galaxy-proftpd:for_galaxy_v$GALAXY_VER_FOR_POSTGRES
 
+function log() {
+  echo -e "$@" >&2
+}
+
+# Error {{{1
+################################################################
+
+function error {
+
+	local msg=$1
+	local code=$2
+	[[ -z $code ]] && code=1
+
+	log "[ERROR] $msg"
+
+	exit $code
+}
+
+
 # Debug {{{1
 ################################################################
 
@@ -86,7 +91,7 @@ function debug {
     local level=$2
     [[ -n $level ]] || level=1
 
-    [[ $DEBUG -lt $level ]] || echo "[DEBUG] $msg" >&2
+    [[ $DEBUG -lt $level ]] || log "[DEBUG] $msg"
 }
 
 # Warning {{{1
@@ -96,28 +101,30 @@ function warning {
 
     local msg=$1
 
-    echo "[WARNING] ***** $msg *****" >&2
+    log "[WARNING] ##### $msg #####"
 }
 
 # Print help {{{1
 ################################################################
 
 function print_help {
-    echo "Usage: $SCRIPT_NAME [options]"
-    echo
-    echo "Build PhenoMeNal Galaxy docker images."
-    echo
-    echo "Options:"
-    echo "   -g, --debug                 Debug mode."
-    echo "   -h, --help                  Print this help message."
-    echo "       --init-tag       <tag>  Set the tag for the Galaxy Init Flavour container image."
-    echo "   -p, --push                  Push dockers. You can also set environment variable DOCKER_PUSH_ENABLED to \"true\". Enabled by default."
-    echo "   +p, --no-push               Do not push dockers. You can also set environment variable DOCKER_PUSH_ENABLED to \"false\"."
-    echo "       --postgres-tag   <tag>  Set the tag for the Galaxy Postgres container image."
-    echo "       --proftpd-tag    <tag>  Set the tag for the Galaxy Proftpd container image."
-    echo "   -u, --container-user <user> Set the container user. You can also set the environment variable CONTAINER_USER."
-    echo "       --web-tag        <tag>  Set the tag for the Galaxy Web k8s container image."
-    echo
+    (
+        echo "Usage: $SCRIPT_NAME [options]"
+        echo
+        echo "Build PhenoMeNal Galaxy docker images."
+        echo
+        echo "Options:"
+        echo "   -g, --debug                 Debug mode."
+        echo "   -h, --help                  Print this help message."
+        echo "       --init-tag       <tag>  Set the tag for the Galaxy Init Flavour container image."
+        echo "   -p, --push                  Push dockers. You can also set environment variable DOCKER_PUSH_ENABLED to \"true\". Enabled by default."
+        echo "   +p, --no-push               Do not push dockers. You can also set environment variable DOCKER_PUSH_ENABLED to \"false\"."
+        echo "       --postgres-tag   <tag>  Set the tag for the Galaxy Postgres container image."
+        echo "       --proftpd-tag    <tag>  Set the tag for the Galaxy Proftpd container image."
+        echo "   -u, --container-user <user> Set the container user. You can also set the environment variable CONTAINER_USER."
+        echo "       --web-tag        <tag>  Set the tag for the Galaxy Web k8s container image."
+        echo
+    ) >&2
 }
 
 # Read args {{{1
@@ -160,6 +167,7 @@ function read_args {
     debug "Argument GALAXY_WEB_K8S_TAG=$GALAXY_WEB_K8S_TAG"
     debug "Argument POSTGRES_TAG=$POSTGRES_TAG"
     debug "Argument PROFTPD_TAG=$PROFTPD_TAG"
+    debug "shift_count=$shift_count"
 }
 
 # MAIN {{{1
@@ -169,11 +177,11 @@ read_args "$@"
 
 if [ -n $ANSIBLE_REPO ]
     then
-       echo "Making custom galaxy-base-pheno:$TAG from $ANSIBLE_REPO at $ANSIBLE_RELEASE"
+       log "Making custom galaxy-base-pheno:$TAG from $ANSIBLE_REPO at $ANSIBLE_RELEASE"
        docker build $NO_CACHE --build-arg ANSIBLE_REPO=$ANSIBLE_REPO --build-arg ANSIBLE_RELEASE=$ANSIBLE_RELEASE -t $GALAXY_BASE_PHENO_TAG docker-galaxy-stable/compose/galaxy-base/
        if [[ ! -z ${PUSH_INTERMEDIATE_IMAGES:-} ]];
        then
-         echo "Pushing intermediate image $DOCKER_REPO$DOCKER_USER/galaxy-base-pheno:$TAG"
+         log "Pushing intermediate image $DOCKER_REPO$DOCKER_USER/galaxy-base-pheno:$TAG"
            docker push $GALAXY_BASE_PHENO_TAG
        fi
 fi
@@ -181,19 +189,19 @@ fi
 
 if [ -n $GALAXY_REPO ]
     then
-       echo "Making custom galaxy-init-pheno:$TAG from $GALAXY_REPO at $GALAXY_RELEASE"
+       log "Making custom galaxy-init-pheno:$TAG from $GALAXY_REPO at $GALAXY_RELEASE"
        DOCKERFILE_INIT_1=Dockerfile
        if [ -n $ANSIBLE_REPO ]
        then
            sed s+$GALAXY_BASE_FROM_TO_REPLACE+$GALAXY_BASE_PHENO_TAG+ docker-galaxy-stable/compose/galaxy-init/Dockerfile > docker-galaxy-stable/compose/galaxy-init/Dockerfile_init
            FROM=$(grep ^FROM docker-galaxy-stable/compose/galaxy-init/Dockerfile_init | awk '{ print $2 }')
-           echo "Using FROM $FROM for galaxy init"
+           log "Using FROM $FROM for galaxy init"
            DOCKERFILE_INIT_1=Dockerfile_init
        fi
        docker build $NO_CACHE --build-arg GALAXY_REPO=$GALAXY_REPO --build-arg GALAXY_RELEASE=$GALAXY_RELEASE --build-arg ISATOOLS_LITE_INSTALL=True -t $GALAXY_INIT_PHENO_TAG -f docker-galaxy-stable/compose/galaxy-init/$DOCKERFILE_INIT_1 docker-galaxy-stable/compose/galaxy-init/
        if [[ ! -z ${PUSH_INTERMEDIATE_IMAGES:-} ]];
        then
-           echo "Pushing intermediate image $GALAXY_INIT_PHENO_TAG"
+           log "Pushing intermediate image $GALAXY_INIT_PHENO_TAG"
            docker push $GALAXY_INIT_PHENO_TAG
        fi
 fi
@@ -201,7 +209,7 @@ fi
 
 if [ -n $GALAXY_REPO ]
 then
-  echo "Making custom galaxy-init-pheno-flavoured:$TAG starting from galaxy-init-pheno:$TAG"
+  log "Making custom galaxy-init-pheno-flavoured:$TAG starting from galaxy-init-pheno:$TAG"
   sed s+pcm32/galaxy-init-pheno$+$GALAXY_INIT_PHENO_TAG+ $DOCKERFILE_INIT_FLAVOUR > Dockerfile_init_tagged
   DOCKERFILE_INIT_FLAVOUR=Dockerfile_init_tagged
 fi
@@ -214,7 +222,7 @@ fi
 
 if [ -n $GALAXY_REPO ]
 then
-  echo "Making custom galaxy-web-k8s:$TAG from $GALAXY_REPO at $GALAXY_RELEASE"
+  log "Making custom galaxy-web-k8s:$TAG from $GALAXY_REPO at $GALAXY_RELEASE"
   sed s+$GALAXY_BASE_FROM_TO_REPLACE+$GALAXY_BASE_PHENO_TAG+ docker-galaxy-stable/compose/galaxy-web/Dockerfile > docker-galaxy-stable/compose/galaxy-web/Dockerfile_web
   DOCKERFILE_WEB=Dockerfile_web
 fi
@@ -239,12 +247,12 @@ if [[ "${DOCKER_PUSH_ENABLED:-}" = "true" ]]; then
 fi
 
 
-echo "Relevant containers:"
+log "Relevant containers:"
 echo "Web:          $GALAXY_WEB_K8S_TAG"
 echo "Init Flavour: $GALAXY_INIT_PHENO_FLAVOURED_TAG"
 echo "Postgres:     $POSTGRES_TAG"
 echo "Proftpd:      $PROFTPD_TAG"
 
 if [[ "${DOCKER_PUSH_ENABLED:-}" != "true" ]]; then
-  echo "Container images not pushed.  They are only available in your current Docker daemon"
+  log "Container images not pushed.  They are only available in your current Docker daemon"
 fi
