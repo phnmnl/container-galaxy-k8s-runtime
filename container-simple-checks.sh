1ef86d5a4090dbfce58a5c715d0a71243199d35f
Luca Pireddu <pireddu@crs4.it>
2019-07-03 17:15:05 +0200

    correct galaxy path in container for simple checks (#342)

--- container-simple-checks.sh
+++ container-simple-checks.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 
-DIRECTORY=/galaxy-export
+DIRECTORY=/galaxy-central
 if [ ! -d "$DIRECTORY" ]; then
    echo "Galaxy directory $DIRECTORY not found, test failed"
    exit 1
 fi
 
-RUNSCRIPT=/${DIRECTORY}/run.sh
+RUNSCRIPT="${DIRECTORY}/run.sh"
 if [ ! -x "$RUNSCRIPT" ]; then
    echo "Galaxy $RUNSCRIPT executable script not found or not executable"
    exit 1 
